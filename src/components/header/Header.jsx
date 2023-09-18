84f82f6f099196b13b862f343edf82d34ae566bc
Rndmun <91401175+Rndmun@users.noreply.github.com>
2023-09-17 23:49:45 -0700

    header updated skeleton

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { motion, AnimatePresence } from 'framer-motion'; // Import Framer Motion components
 import './Header.css';
 import '../../App.css';
 import logo from '../../assets/Logo.png';
@@ -21,7 +22,7 @@ const Header = () => {
       label: 'Our Range',
       submenu: [
         { label: <img src={placeholder} alt="Placeholder" />, link: '/stormbreaker19' },
-        { label:<img src={placeholder} alt="Placeholder" />, link: '/stormbreaker21' },
+        { label: <img src={placeholder} alt="Placeholder" />, link: '/stormbreaker21' },
       ],
     },
     {
@@ -46,7 +47,13 @@ const Header = () => {
   ];
 
   return (
-    <header className="header">
+    <motion.header
+      className="header"
+      initial={{ backgroundColor: 'transparent' }}
+      whileHover={{ backgroundColor: 'white', color: 'black' }}
+      animate={activeMenu ? { backgroundColor: 'white', color: 'black' } : { backgroundColor: 'transparent' }}
+      transition={{ duration: 0.5 }}
+    >
       <div className="header-logo">
         <Link to="/">
           <img src={logo} alt="" height={50} />
@@ -55,25 +62,43 @@ const Header = () => {
       <nav className="header-nav">
         <ul className="menu">
           {menuData.map((menu, index) => (
-            <li
+            <motion.li
               key={index}
               className={`menu-item ${activeMenu === menu.label ? 'active' : ''}`}
               onMouseEnter={() => handleMenuHover(menu.label)}
               onMouseLeave={handleMenuLeave}
+              initial={{ opacity: 1 }} // Initial opacity set to 1
+              animate={{ opacity: 1 }} // Opacity remains 1
+              exit={{ opacity: 0 }} // Exit opacity set to 0
             >
               <button>
                 <span>{menu.label}</span>
               </button>
-              {activeMenu === menu.label && menu.submenu && (
-                <div className="sub-menu">
-                  {menu.submenu.map((subitem, subIndex) => (
-                    <Link key={subIndex} to={subitem.link}>
-                      <div className="sub-menu-item">{subitem.label}</div>
-                    </Link>
-                  ))}
-                </div>
-              )}
-            </li>
+              <AnimatePresence>
+                {activeMenu === menu.label && menu.submenu && (
+                  <motion.div
+                    className="sub-menu"
+                    initial={{ opacity: 0, scaleY: 0, transformOrigin: 'top' }} // Initial scaleY set to 0
+                    animate={{ opacity: 1, scaleY: 1 }} // ScaleY set to 1
+                    exit={{ opacity: 0, scaleY: 0 }} // Exit scaleY set to 0
+                    transition={{ duration: 0.3, delay: 0.2 }} // Adjust duration and delay as needed
+                  >
+                    {menu.submenu.map((subitem, subIndex) => (
+                      <Link key={subIndex} to={subitem.link}>
+                        <motion.div
+                          className="sub-menu-item"
+                          initial={{ opacity: 0, y: -10 }}
+                          animate={{ opacity: 1, y: 0 }}
+                          transition={{ duration: 0.3, delay: 0.5 + subIndex * 0.1 }}
+                        >
+                          {subitem.label}
+                        </motion.div>
+                      </Link>
+                    ))}
+                  </motion.div>
+                )}
+              </AnimatePresence>
+            </motion.li>
           ))}
           <li className="menu-item">
             <Link to="/contact">
@@ -82,9 +107,8 @@ const Header = () => {
           </li>
         </ul>
       </nav>
-    </header>
+    </motion.header>
   );
 };
-
 
 export default Header;
