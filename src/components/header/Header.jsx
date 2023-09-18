c8c3d7f0299c88b7a6e6da74d2df1006efe60db1
Rndmun <91401175+Rndmun@users.noreply.github.com>
2023-09-18 08:53:09 -0700

    freaking header test

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -51,7 +51,7 @@ const Header = () => {
       initial={{ backgroundColor: 'transparent' }}
       whileHover={{ backgroundColor: 'white', color: 'black' }}
       animate={activeMenu ? { backgroundColor: 'white', color: 'black' } : { backgroundColor: 'transparent' }}
-      transition={{ duration: 0.5 }}
+      transition={{ duration: 0.3 }}
     >
       <div className="header-logo">
         <Link to="/">
