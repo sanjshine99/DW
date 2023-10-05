64343a40177f55ca16d8fbe6c0e12c125216ab2a
Rndmun <91401175+Rndmun@users.noreply.github.com>
2023-10-05 08:03:41 -0700

    changes

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -11,27 +11,7 @@ const Footer = () => {
   return (
     <footer className="footer">
       {/* Social Icons Section */}
-      <div className="social-icons">
-        <div className="social-links">
-          <a href="https://www.instagram.com/deluxecaravansaustralia/" target="_blank" rel="noopener noreferrer">
-            <img src={instagram} alt="Instagram" className='icon' />
-          </a>
-          <a href="https://www.facebook.com/deluxecaravansaustralia" target="_blank" rel="noopener noreferrer">
-            <img src={facebook} alt="Facebook" className='icon' />
-          </a>
-          <a href="https://twitter.com/CaravansDeluxe" target="_blank" rel="noopener noreferrer">
-            <img src={twitter} alt="Twitter" className='icon' />
-          </a>
-          <a href="https://www.tiktok.com/@deluxecaravansaustralia" target="_blank" rel="noopener noreferrer">
-            <img src={tiktok} alt="TikTok" className='icon' />
-          </a>
-          <a href="https://www.youtube.com/watch?v=a_KE1CVPT48" target="_blank" rel="noopener noreferrer">
-            <img src={youtube} alt="YouTube" className='icon' />
-          </a>
-         
-        </div>
-       
-      </div>
+     
       {/* Footer Links Section */}
       <div className="footer-links">
       <div className="logo">
@@ -48,7 +28,7 @@ const Footer = () => {
           <a href="/#/about" className="footer-link">About us</a>
           <a href="#" className="footer-link">Warranty policy</a>
           <a href="/#/blog" className="footer-link">Blog</a>
-          <a href="#" className="footer-link">Video</a>
+          <a href="/#/video" className="footer-link">Video</a>
         </div>
         <div className="footer-links-column">
           <div className="footer-links-header">Contact</div>
@@ -58,10 +38,32 @@ const Footer = () => {
         </div>
       </div>
       
-      {/* Copyright Section */}
-      <div className="copyright">
-      <hr></hr>
-        © {new Date().getFullYear()} Copyright: Deluxe Caravans
+       <hr className="footer-hr" />
+
+      {/* Copyright and Social Icons Section */}
+      <div className="copyright-and-social">
+        <div className="copyright">
+          © {new Date().getFullYear()} Copyright: Deluxe Caravans
+        </div>
+        <div className="social-icons">
+          <div className="social-links">
+            <a href="https://www.instagram.com/deluxecaravansaustralia/" target="_blank" rel="noopener noreferrer">
+              <img src={instagram} alt="Instagram" className='icon' />
+            </a>
+            <a href="https://www.facebook.com/deluxecaravansaustralia" target="_blank" rel="noopener noreferrer">
+              <img src={facebook} alt="Facebook" className='icon' />
+            </a>
+            <a href="https://twitter.com/CaravansDeluxe" target="_blank" rel="noopener noreferrer">
+              <img src={twitter} alt="Twitter" className='icon' />
+            </a>
+            <a href="https://www.tiktok.com/@deluxecaravansaustralia" target="_blank" rel="noopener noreferrer">
+              <img src={tiktok} alt="TikTok" className='icon' />
+            </a>
+            <a href="https://www.youtube.com/watch?v=a_KE1CVPT48" target="_blank" rel="noopener noreferrer">
+              <img src={youtube} alt="YouTube" className='icon' />
+            </a>
+          </div>
+        </div>
       </div>
     </footer>
   );
--- src/components/video/Video.jsx
+++ src/components/video/Video.jsx
@@ -1,15 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../home/Home.css';
 import './video.css';
 
 function Video() {
-  return (
-    <div className='container'>
-      <div className='component'>
-        Video
+    // Sample video data (replace with your own data)
+    const videos = [
+      {
+        title: 'We Built A $120,000 CARAVAN - Storm breaker.. Launch Video!!',
+        description: 'Our first look at the most eco-friendly optioned caravan in the market. We offer more than just a great-looking caravan. Peak Performance and Efficiency.',
+        videoId: 'a_KE1CVPT48?si=xmPrmqL9fsyB3OIr',
+      },
+      {
+        title: 'Video 2',
+        description: 'This is the description for Video 2.',
+        videoId: 'VIDEO_ID_2',
+      },
+      {
+        title: 'Video 3',
+        description: 'This is the description for Video 3.',
+        videoId: 'VIDEO_ID_3',
+      },
+      {
+        title: 'Video 4',
+        description: 'This is the description for Video 3.',
+        videoId: 'VIDEO_ID_4',
+      },
+      {
+        title: 'Video 5',
+        description: 'This is the description for Video 3.',
+        videoId: 'VIDEO_ID_5',
+      },
+    ];
+  
+    return (
+      <div className="container">
+        <div className="component">
+          <h1>Our Videos</h1>
+      <div className="video-gallery">
+        <div className="video-grid">
+          {videos.map((video, index) => (
+            <div key={index} className="video-card">
+              <iframe
+                title="YouTube Video"
+                width="300"
+                height="169"
+                src={`https://www.youtube.com/embed/${video.videoId}`}
+                frameBorder="0"
+                allowFullScreen
+              ></iframe>
+              <h3>{video.title}</h3>
+              <p>{video.description}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
-  )
-}
+  </div>
+    );
+  };
 
 export default Video
