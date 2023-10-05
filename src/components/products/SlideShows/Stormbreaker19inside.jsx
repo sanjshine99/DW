3231af700b9fc876b9ccb4f532b27c36f8db192c
Rndmun <91401175+Rndmun@users.noreply.github.com>
2023-10-05 08:54:04 -0700

    Product gallery changes

--- src/components/products/SlideShows/Stormbreaker19inside.jsx
+++ src/components/products/SlideShows/Stormbreaker19inside.jsx
@@ -1,9 +1,32 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./Stormbreaker19inside.css";
-import SB196Ext from '../../../assets/Product renders/SB196Exterior.png';
-import SB196layout from '../../../assets/Product renders/SB196 layout.png';
+import SB196Ext from "../../../assets/Product renders/SB196Exterior.png";
+import SB196layout from "../../../assets/Product renders/SB196 layout.png";
+
+
+const sections = [
+  {
+    name: "Exterior",
+    images: [SB196Ext, SB196layout],
+    description: "Exterior section description goes here.",
+  },
+  {
+    name: "Layout",
+    images: [SB196layout, SB196Ext],
+    description: "Layout section description goes here.",
+  },
+  {
+    name: "Inside",
+    images: [SB196Ext], // Use imported images or URLs consistently
+    description: "Inside section description goes here.",
+  },
+];
 
 function Strombreaker19inside() {
+
+  const [activeSection, setActiveSection] = useState(null);
+  const [activeImageIndex, setActiveImageIndex] = useState(0);
+
   useEffect(() => {
     var $cont = document.querySelector(".cont_19_i");
     var $elsArr = [].slice.call(document.querySelectorAll(".el_19_i"));
@@ -38,98 +61,68 @@ function Strombreaker19inside() {
     });
   }, []);
 
+  const handleNextImage = () => {
+    const maxIndex = sections[activeSection].images.length - 1;
+    setActiveImageIndex((prevIndex) =>
+      prevIndex < maxIndex ? prevIndex + 1 : 0
+    );
+  };
+
+  const handlePrevImage = () => {
+    const maxIndex = sections[activeSection].images.length - 1;
+    setActiveImageIndex((prevIndex) =>
+      prevIndex > 0 ? prevIndex - 1 : maxIndex
+    );
+  };
+
+
   return (
     <div className="cont_19_i s--inactive">
-      <div className="cont__inner_19_i">
-        <div className="el_19_i">
+    <div className="cont__inner_19_i">
+      {sections.map((section, index) => (
+        <div
+          key={index}
+          className={`el_19_i ${
+            activeSection === index ? "s--active" : ""
+          }`}
+        >
           <div className="el__overflow_19_i">
             <div className="el__inner_19_i">
               <div className="el__bg_19_i">
-                <img
-                  className="el__bg_19_i"
-                  src={SB196Ext}
-                  alt="1"
-                />
+                {/* Slideshow Container */}
+                <div className="slideshow-container">
+                  {section.images.map((image, imgIndex) => (
+                    <div
+                      key={imgIndex}
+                      className={`mySlides ${
+                        imgIndex === activeImageIndex ? "active" : ""
+                      }`}
+                    >
+                      <img src={image} alt={`Image ${imgIndex + 1}`} />
+                    </div>
+                  ))}
+                  <button className="prev" onClick={handlePrevImage}>
+                    &#10094; Previous
+                  </button>
+                  <button className="next" onClick={handleNextImage}>
+                    Next &#10095;
+                  </button>
+                </div>
               </div>
               <div className="el__preview-cont_19_i">
-                <h2 className="el__heading_19_i">Exterior</h2>
+                <h2 className="el__heading_19_i">{section.name}</h2>
               </div>
               <div className="el__content_19_i">
-                 <div className="el__text_19_i">Identically designed to the Storm breaker 21’6 with its solid, matte finish, our family caravan isn’t just a treat on the inside but is extremely easy on the eyes too. Designed to take on the diversity of Australia’s landscape and terrain with a high checker plate, the Storm breaker possesses advanced mechanical systems including a powerful 6“chassis and OFF-ROAD suspension system.
-                    Enjoy powerful features like our heavy-duty 16” wheels and family-friendly add-ons like a foldable picnic table, bike rack, large toolbox to enjoy a caravan experience you’ve only seen in movies.</div>
+                <div className="el__text_19_i">{section.description}</div>
                 <div className="el__close-btn_19_i"></div>
               </div>
             </div>
           </div>
-          {/* <div className="el__index_19_i">
-                <div className="el__index-back_19_i">1</div>
-                <div className="el__index-front_19_i">
-                  <div className="el__index-overlay_19_i" data-index="1">
-                    1
-                  </div>
-                </div>
-              </div> */}
         </div>
-        <div className="el_19_i">
-          <div className="el__overflow_19_i">
-            <div className="el__inner_19_i">
-              <div className="el__bg_19_i">
-                <img
-                  className="el__bg_19_i"
-                  src={SB196layout}
-                  alt="2"
-                />
-              </div>
-              <div className="el__preview-cont_19_i">
-                <h2 className="el__heading_19_i">Layout</h2>
-              </div>
-              <div className="el__content_19_i">
-                <div className="el__text_19_i">The Storm breaker 19’6 provides you with all the worldly comforts you can expect when you’re on the road. With its plush and compact East-West sleeping arrangements for the main bed and spacious bunks for the kids, large living quarters, and fully equipped angled - kitchen. Rnjoy every moment of your adventures with our world-class features.
-                We consider everything too; from your TV to air conditioning, water heating and angled kitchen bench, you have everything you need to take a piece of home with you on every trip.</div> 
-                <div className="el__close-btn_19_i"></div>
-              </div>
-            </div>
-          </div>
-          {/* <div className="el__index_19_i">
-                <div className="el__index-back_19_i">2</div>
-                <div className="el__index-front_19_i">
-                  <div className="el__index-overlay_19_i" data-index="2">
-                    2
-                  </div>
-                </div>
-              </div> */}
-        </div>
-        <div className="el_19_i">
-          <div className="el__overflow_19_i">
-            <div className="el__inner_19_i">
-              <div className="el__bg_19_i">
-                <img
-                  className="el__bg_19_i"
-                  src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/142996/onepgscr-5.jpg"
-                  alt="3"
-                />
-              </div>
-              <div className="el__preview-cont_19_i">
-                <h2 className="el__heading_19_i">Inside</h2>
-              </div>
-              <div className="el__content_19_i">
-                {/* <div className="el__text_19_i">Whatever</div> */}
-                <div className="el__close-btn_19_i"></div>
-              </div>
-            </div>
-          </div>
-          {/* <div className="el__index_19_i">
-                <div className="el__index-back_19_i">3</div>
-                <div className="el__index-front_19_i">
-                  <div className="el__index-overlay_19_i" data-index="3">
-                    3
-                  </div>
-                </div>
-              </div> */}
-        </div>
-      </div>
+      ))}
     </div>
-  );
+  </div>
+);
 }
 
 export default Strombreaker19inside;
