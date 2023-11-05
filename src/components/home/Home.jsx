cdd0254b137aca486dcbb0a24a51ca510156f7d6
Rndmun <91401175+Rndmun@users.noreply.github.com>
2023-11-05 08:45:15 -0800

    final version changes

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Home.css";
 import Video from "./homecomponents/Video_landing";
 import Gallery from "./homecomponents/Gallery_landing";
 import Reviews from "./homecomponents/CustomerReviews";
 import Partners from "./homecomponents/Partners";
 import HomeDesc from "./homecomponents/HomeDescription";
+import Model from "../popup-ad/model";
 
 function Home() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    const lastShownTimestamp = localStorage.getItem("adLastShownTimestamp");
+    const currentTime = Date.now();
+    const ONE_MINUTE = 60 * 1000;
+
+    if (!lastShownTimestamp || currentTime - lastShownTimestamp >= ONE_MINUTE) {
+      setIsOpen(true);
+      localStorage.setItem("adLastShownTimestamp", currentTime);
+    }
+  }, []);
+
+  const handleCloseModal = () => {
+    setIsOpen(false);
+  };
+
   return (
     <div className="container">
+      <div className="ad-model">
+        <Model isOpen={isOpen} onClose={handleCloseModal} />
+      </div>
       <div className="videocomponent">
         <Video />
       </div>
@@ -19,7 +40,7 @@ function Home() {
         <Gallery />
       </div>
       <div className="component">
-      <Reviews />
+        <Reviews />
       </div>
       <div className="partnercomponent">
         <Partners />
