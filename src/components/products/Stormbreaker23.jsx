7d8c5b7dc4a82e4330c97c3c74ef99cb8855616e
Sanjshine99 <sanubandara12639@gmail.com>
2023-12-14 22:07:00 +0530

    fix(): add inline styleing to fix div postion issue, removed unwanted commented code

--- src/components/products/Stormbreaker23.jsx
+++ src/components/products/Stormbreaker23.jsx
@@ -61,7 +61,10 @@ const Stormbreaker23 = () => {
     <div className="container">
       <div className="component">
         <div className="background-image">
-          <img src="https://deluxcaravan.b-cdn.net/assets/Product%20renders/SB2311Exterior.webp" alt="" />
+          <img
+            src="https://deluxcaravan.b-cdn.net/assets/Product%20renders/SB2311Exterior.webp"
+            alt=""
+          />
           <div className="image-overlay">
             <div className="button-container">
               <h1 className="Product-header revealUp">Stormbreaker23`11</h1>
@@ -88,56 +91,50 @@ const Stormbreaker23 = () => {
       {/* <div className='slideshowcomopnent'>
     </div> */}
       <div className="component">
-        <div className="product-container" id="layout">
+        <div
+          className="product-container"
+          id="layout"
+          style={{
+            gridGap: "100px",
+          }}
+        >
           <div className="description">
             <p>
-            Got more than 3 kids or always wanted buy a caravan with a spacious living space for your kids. Look
-no further than our Stormbreaker 23&#39;11, the solution you&#39;ve been seeking. This model offers all the
-remarkable features unique to this family bunk model. A key feature that set this model apart it the
-large, separate ensuite at the rear. Built with premium interior, exterior, and mechanical features
-when you embark on the open road with your loved ones, you&#39;ll be treated to an exceptionally
-roomy layout, complete with all the amenities you&#39;d expect from a luxury RV.
+              Got more than 3 kids or always wanted buy a caravan with a
+              spacious living space for your kids. Look no further than our
+              Stormbreaker 23&#39;11, the solution you&#39;ve been seeking. This
+              model offers all the remarkable features unique to this family
+              bunk model. A key feature that set this model apart it the large,
+              separate ensuite at the rear. Built with premium interior,
+              exterior, and mechanical features when you embark on the open road
+              with your loved ones, you&#39;ll be treated to an exceptionally
+              roomy layout, complete with all the amenities you&#39;d expect
+              from a luxury RV.
             </p>
-            <div className="package-details">
-            <p>
-                <h2>NOW WITH ALL 2024 ORDERS, OFF-GRID PACKAGE INCLUSIONS: </h2>
-              </p>
-              <p>
-              <h3>WORTH $5000 !</h3>
-              </p>
-              <ul>
-  <li>DOMETIC FRESH JET AIRCON</li>
-  <li>188L DOMETIC COMPRESSOR FRIDGE</li>
-  <li>3 X 170W SOLAR PANELS</li>
-  <li>2 X 100AH LITHIUM BATTERIES</li>
-</ul>
-
-            </div>
-            {/* <p>
-              <button className="virtual-btn">
-                <a href="/tour">Have a look at STORMBREAKER 21`6</a>
-              </button>
-            </p> */}
             <button
               className="btn hover-border-1 revealUp"
               onClick={openWarrantyPopupWarrantyPolicy}
             >
-               Download Our Brochure and Upgrade List
+              Download Our Brochure and Upgrade List
             </button>
             {isWarrantyPopupOpen && (
               <WarrantyPolicyPopup onClose={closeWarrantyPopupWarrantyPolicy} />
             )}
           </div>
-          {/* <div className="image">
-            <iframe
-              src="https://esceneyf.sirv.com/Spins/test2/test2.spin"
-              title="Stormbreaker21"
-              width="1000px"
-              height="1000px"
-              frameborder="0"
-              allowFullScreen
-            ></iframe>{" "}
-          </div> */}
+          <div className="package-details">
+            <p>
+              <h2>NOW WITH ALL 2024 ORDERS, OFF-GRID PACKAGE INCLUSIONS: </h2>
+            </p>
+            <p>
+              <h3>WORTH $5000 !</h3>
+            </p>
+            <ul>
+              <li>DOMETIC FRESH JET AIRCON</li>
+              <li>188L DOMETIC COMPRESSOR FRIDGE</li>
+              <li>3 X 170W SOLAR PANELS</li>
+              <li>2 X 100AH LITHIUM BATTERIES</li>
+            </ul>
+          </div>
         </div>
       </div>
       <div className="component" id="specs">
@@ -147,9 +144,9 @@ roomy layout, complete with all the amenities you&#39;d expect from a luxury RV.
         <h1>Specs Menu</h1>
         <Storm23specs />
       </div>
-      <GoToTop/>
+      <GoToTop />
     </div>
-   );
-  };
-  
-  export default React.memo(Stormbreaker23);
+  );
+};
+
+export default React.memo(Stormbreaker23);
