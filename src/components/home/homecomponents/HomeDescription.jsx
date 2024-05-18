95110dd87e71e2ae44ccdd855f92dae6920bb159
Sanjshine99 <sanubandara12639@gmail.com>
2024-05-18 20:00:04 +0530

    fix(): promo video

--- src/components/home/homecomponents/HomeDescription.jsx
+++ src/components/home/homecomponents/HomeDescription.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { useInView } from 'react-intersection-observer';
-import './HomeComponent.css';
+import React from "react";
+import { useInView } from "react-intersection-observer";
+import "./HomeComponent.css";
 
 const HomeDescription = () => {
   const stagger = 0.5;
@@ -8,7 +8,7 @@ const HomeDescription = () => {
   const [ref, inView] = useInView({
     triggerOnce: true,
     threshold: 0.2,
-    rootMargin: '0px 0px -200px 0px',
+    rootMargin: "0px 0px -200px 0px",
   });
 
   return (
@@ -16,26 +16,47 @@ const HomeDescription = () => {
       className="container"
       style={{
         opacity: inView ? 1 : 0,
-        transition: 'opacity 1.5s',
+        transition: "opacity 1.5s",
       }}
       ref={ref}
     >
       <div className="desccomponent">
-        <h1 style={{ opacity: 1, transform: 'translateY(0)' }}>
-          CROSS COUNTRY? WE HEAR YOU!
-        </h1>
-        <p style={{ opacity: 1, transform: 'translateY(0)', transitionDelay: stagger * 1 + 's' }}>
-          Hitting the road for a cross-country adventure or a quick escape from suburbia should be an experience you enjoy.
+        <p
+          style={{
+            opacity: 1,
+            transform: "translateY(0)",
+            transitionDelay: stagger * 1 + "s",
+          }}
+        >
+          Hitting the road for a cross-country adventure or a quick escape from
+          suburbia should be an experience you enjoy.
         </p>
-        <p style={{ opacity: 1, transform: 'translateY(0)', transitionDelay: stagger * 2 + 's' }}>
-          What you're looking for is a getaway with modern comforts and amenities with your loved ones; a place that feels like home—even if it's on wheels.
+        <p
+          style={{
+            opacity: 1,
+            transform: "translateY(0)",
+            transitionDelay: stagger * 2 + "s",
+          }}
+        >
+          What you're looking for is a getaway with modern comforts and
+          amenities with your loved ones; a place that feels like home—even if
+          it's on wheels.
         </p>
-        <p style={{ opacity: 1, transform: 'translateY(0)', transitionDelay: stagger * 3 + 's' }}>
-          At Deluxe Caravans, we help you fall in love with life on the road. With our range of luxury caravans for families, couples, and free-spirited nomads, take on the great outdoors with all the comfort and safety you deserve.
+        <p
+          style={{
+            opacity: 1,
+            transform: "translateY(0)",
+            transitionDelay: stagger * 3 + "s",
+          }}
+        >
+          At Deluxe Caravans, we help you fall in love with life on the road.
+          With our range of luxury caravans for families, couples, and
+          free-spirited nomads, take on the great outdoors with all the comfort
+          and safety you deserve.
         </p>
       </div>
     </div>
   );
-}
+};
 
 export default React.memo(HomeDescription);
--- src/components/home/homecomponents/Promovid.jsx
+++ src/components/home/homecomponents/Promovid.jsx
@@ -5,18 +5,30 @@ import "./HomeComponent.css";
 
 gsap.registerPlugin(ScrollTrigger);
 
-function Promovid() { return (
-  <div style={{ padding: '56.25% 0 0 0', position: 'relative' }}>
-    <iframe
-      src="https://player.vimeo.com/video/945631996?h=7774de72a1&badge=0&autopause=0&player_id=0&app_id=58479"
-      style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
-      frameBorder="0"
-      allow="autoplay; fullscreen; picture-in-picture"
-      allowFullScreen
-      title="Vimeo Video"
-    ></iframe>
-  </div>
-);
+function Promovid() {
+  return (
+    <div className="container">
+      <h1 style={{ opacity: 1, transform: "translateY(0)" }}>
+        CROSS COUNTRY? WE HEAR YOU!
+      </h1>
+      <div style={{ padding: "56.25% 0 0 0", position: "relative" }}>
+        <iframe
+          src="https://player.vimeo.com/video/945631996?h=7774de72a1&badge=0&autopause=0&player_id=0&app_id=58479"
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+          }}
+          frameBorder="0"
+          allow="autoplay; fullscreen; picture-in-picture"
+          allowFullScreen
+          title="Vimeo Video"
+        ></iframe>
+      </div>
+    </div>
+  );
 }
 
 export default Promovid;
